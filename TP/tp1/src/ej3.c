d1bc4de6e9cb4e1b9387d0d06a1b4b48d74cc736
ljcmps01 <ljcmps01@gmail.com>
2024-06-14 07:12:42 -0300

    ejercicio_3: ejercicio 3 finalizado
    3. De un alumno del curso de ingreso a la UTN se ingresan las notas obtenidas en los
    exámenes de las 3 materias que lo forman. Calcular el promedio del alumno y mostrarlo
    por pantalla.
    
    file:src/ej3.c, build/bin/ej3

---
+++ TP/tp1/src/ej3.c
@@ -0,0 +1,36 @@
+/* 
+3. De un alumno del curso de ingreso a la UTN se ingresan las notas obtenidas en los 
+exámenes de las 3 materias que lo forman. Calcular el promedio del alumno y mostrarlo 
+por pantalla
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N_NOTAS 3
+
+int main()
+{
+    int nota, sumatoria=0, i=0;
+    float promedio;
+
+
+    do
+    {
+        printf("\nIngrese la nota %d: ", i+1);
+        scanf("%d", &nota);
+        if(nota>0)
+        {
+            sumatoria += nota;
+            i++;
+        }
+        else
+            printf("\nIngrese una nota valida");
+    }while(i < N_NOTAS);
+
+    promedio = (float)sumatoria / 3;
+
+    printf("\nEl promedio es: %.2f\n", promedio);
+
+    return 0;
+}
