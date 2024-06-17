94e17222b4eb4f9b25167194543148ab70c33cc4
Alejo Rodrigo Campos <46141665+ljcmps01@users.noreply.github.com>
2024-06-17 09:10:51 -0300

    Ejercicio 6 (#10)
    * documentacion(README) Agregar enunciados al README
    
    Se transcriben los enunciados del pdf del TP1 al
    README para poder leerlos facilmente desde el CLI
    
    files: README.md
    
    * Estructura (src/*) Agregar archivos de ejercicio
    
    Agregar archivos iniciales para cada enunciado del
    TP, con estructura basica de un archivo .c
    incluyendo un comentario superior con el enunciado
    correspondiente
    
    files: src/*.c
    
    * ejercicio 6: Se completo el ejercicio 6
    
       6- Una compañía de seguros está abriendo un depto. de finanzas y estableció
       unprograma para captar clientes, que consiste en lo siguiente: Si el monto
       por el que seefectúa la fianza es menor que $500000 la cuota a pagar será por
       el 3% del monto, y siel monto es mayor que $500000 la cuota a pagar será el
       2% del monto. La afianzadoradesea determinar cuál será la cuota que debe
       pagar un cliente.
    
    file: src/ej6.c
    
    * ejercicio 6: ejercicio 6 completado
    
    6- Una compañía de seguros está abriendo un depto. de finanzas y estableció
       unprograma para captar clientes, que consiste en lo siguiente: Si el monto
       por el que seefectúa la fianza es menor que $500000 la cuota a pagar será por
       el 3% del monto, y siel monto es mayor que $500000 la cuota a pagar será el
       2% del monto. La afianzadoradesea determinar cuál será la cuota que debe
       pagar un cliente.
    
    file: src/ej6.c

--- TP/tp1/src/ej6.c
+++ TP/tp1/src/ej6.c
@@ -7,3 +7,23 @@
    pagar un cliente.
 
 */
+#include <stdio.h>
+#include <stdlib.h>
+
+#define UMBRAL 500000
+
+int main() {
+  float fianza;
+  int supera_umbral, interes[] = {2, 3};
+
+  printf("\nIngrese el valor de la fianza: $");
+  scanf("%f", &fianza);
+
+  // Si el valor de la fianza supera el umbral determinado
+  // por el ejercicio, entonces se usara el valor guardado en el
+  // indice 0 del vector
+  printf("\nLa cuota a pagar es: $%.2f",
+         fianza * (float)interes[fianza < UMBRAL] / 100);
+
+  return EXIT_SUCCESS;
+}
