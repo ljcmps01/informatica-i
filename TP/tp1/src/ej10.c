ec57dcde55e0daa66f1fc00a0c72f02b120bb3ba
ljcmps01 <ljcmps01@gmail.com>
2024-06-17 07:34:36 -0300

    Estructura (src/*) Agregar archivos de ejercicio
    Agregar archivos iniciales para cada enunciado del
    TP, con estructura basica de un archivo .c
    incluyendo un comentario superior con el enunciado
    correspondiente
    
    files: src/*.c

---
+++ TP/tp1/src/ej10.c
@@ -0,0 +1,27 @@
+/*
+10. Tomando como base los resultados obtenidos en un laboratorio de análisis
+clínicos,un médico determina si una persona tiene anemia o no, lo cual depende
+de su nivel dehemoglobina en la sangre, de su edad y de su sexo. Si el nivel de
+hemoglobina que tiene una persona es menor que el rango que le corresponde, se
+determina su resultadocomo positivo y en caso contrario como negativo. La tabla
+en la que el medico se basa para obtener el resultado es la siguiente:
+
+| EDAD              |NIVEL HEMOGLOBINA  |
+| ------------------|------------------:|
+| 0 - 1 mes         |13 - 26 g%         |
+| > 1 y < = 6 meses | 10 - 18 g%        |
+| > 6 y < = 12 meses|  11 - 15 g%       |
+| > 1 y < = 5 años  |  11.5 - 15 g%     |
+| > 5 y < = 10 años |  12.6 - 15.5 g%   |
+| > 10 y < = 15 años|  13 - 15.5 g%     |
+| mujeres > 15 años |  12 - 16 g%       |
+| hombres > 15 años | 14 - 18 g%        |
+
+ */
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+  int foo;
+
+  return EXIT_SUCCESS;
+}
---
+++ TP/tp1/src/ej6.c
@@ -0,0 +1,9 @@
+/*
+   6- Una compañía de seguros está abriendo un depto. de finanzas y estableció
+   unprograma para captar clientes, que consiste en lo siguiente: Si el monto
+   por el que seefectúa la fianza es menor que $500000 la cuota a pagar será por
+   el 3% del monto, y siel monto es mayor que $500000 la cuota a pagar será el
+   2% del monto. La afianzadoradesea determinar cuál será la cuota que debe
+   pagar un cliente.
+
+*/
