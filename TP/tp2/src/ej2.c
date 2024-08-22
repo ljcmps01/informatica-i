870d5e2de79f21d58fc65c1c148e0433aa32daf4
ljcmps01 <ljcmps01@gmail.com>
2024-08-22 10:59:03 +0000

    crear base de ejercicios tp2
    Creacion de los codigo fuente de los enunciados de
    tp2 con el enunciado en la cabecera, librerias
    base y main loop declarado
    
    tp2/src

---
+++ TP/tp2/src/ej2.c
@@ -0,0 +1,14 @@
+/*Ejercicio 2:
+ *
+ * Realizar un programa que:
+ * a- Ingresar una matriz de numeros enteros de 10 x 5 positivos
+ * b- Cambiar una fila por otra
+ * c- Presentar la matriz con su forma
+ */
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+}
---
+++ TP/tp2/src/ej3.c
@@ -0,0 +1,16 @@
+/*Ejercicio 3:
+ *
+ * Ingresar una matriz de 5x6 por filas. Luego presntar
+ * un menu de opciones:
+ * a- Imprimir la matriz en pantalla con su forma
+ * b- Imprimir los elementos positivos y un guion para
+ * los negativos y una X para los ceros
+ * c- Crear y mostrar un vector que contengan los totales por columnas
+ */
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+}
