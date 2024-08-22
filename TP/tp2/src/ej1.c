5bce253414c85881327532666579b7f108023433
ljcmps01 <ljcmps01@gmail.com>
2024-08-22 10:59:03 +0000

    crear base de ejercicios tp2
    Creacion de los codigo fuente de los enunciados de
    tp2 con el enunciado en la cabecera, librerias
    base y main loop declarado
    
    tp2/src

---
+++ TP/tp2/src/ej1.c
@@ -0,0 +1,17 @@
+/*Ejercicio 1:
+ *
+ * Ingresar una matriz de 4x4 por columna
+ * se pide:
+ * a- suma de las columnas con indice impares
+ * b- suma de las filas de indice pares
+ * c- promedio de las columnas pares
+ * d- dada una fila y una columna modificar ese valor
+ */
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+}
