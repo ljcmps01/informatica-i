08666b82f0c0ef1370f78dc39955f078aa612d95
ljcmps01 <ljcmps01@gmail.com>
2024-10-08 16:32:34 +0000

    finalizado ejercicio dos
    2-  Leer 20 números enteros (positivos y negativos) distintos de
    cero. Mostrar el vector tal como fue ingresado y luego mostrar los
    positivos ordenados en forma decreciente y por último mostrar los
    negativos ordenados en forma creciente.

--- TP/tp2/src/utils.c
+++ TP/tp2/src/utils.c
@@ -3,9 +3,37 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * @brief Imprime el array pasado como argumento hasta el elemento len
+ * 
+ * @param arr array a imprimir
+ * @param len cantidad de elementos a mostrar
+ */
 void print_arr(int *arr, int len){
     for(int i = 0; i < len; i++){
         printf("%d ", arr[i]);
     }
     printf("\n");
+}
+
+/**
+ * @brief bubble sort con parametro decreciente/creciente
+ * 
+ * @param arr 
+ * @param len 
+ * @param creciente 
+ *      si es 1 ordena de forma creciente
+ *      si es 0 ordena de forma decreciente
+ */
+void bubble(int *arr, int len, int creciente){
+    int temp;
+    for(int i = 0; i < len - 1; i++){
+        for(int j = 0; j < len - i - 1; j++){
+            if((creciente == 1 && arr[j] > arr[j+1]) || (creciente == 0 && arr[j] < arr[j+1])){
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
 }
