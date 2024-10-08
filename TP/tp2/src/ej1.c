6f9100de406bf4a02707ca3a1dd6fa8643f9eb6d
ljcmps01 <ljcmps01@gmail.com>
2024-10-08 12:40:31 +0000

    Realiza enunciado 1 de TP2
    1-  Leer 20 números enteros positivos, indicar cuál es el mayor y en qué orden fue leído
    (a que elemento pertenece). Mostrar el vector completo.

--- TP/tp2/src/ej1.c
+++ TP/tp2/src/ej1.c
@@ -7,12 +7,24 @@
 
 int main(){
     int numeros[NUM];
+    int flag_primer_entrada = 1;
+    int max,index_max;
 
     for(int i = 0; i < NUM; i++){
         printf("Ingrese un numero: ");
         scanf("%d", &numeros[i]);
-    }
+        if(flag_primer_entrada){
+            max = numeros[i];
+            index_max = i;
+            flag_primer_entrada = 0;
+        }
+        else if(numeros[i] > max){
+            max = numeros[i];
+            index_max = i;
+        }
 
+    }
+    printf("El maximo ingresado es %d con indice %d,\n",max,index_max);
     print_arr(numeros,NUM);
     return EXIT_SUCCESS;
 }
